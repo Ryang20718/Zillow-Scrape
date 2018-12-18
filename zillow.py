d2eaa3765ab2f672bd7acc005a562016c6f2cb85
Ryan Yang <Ryanyang99@hotmail.com>
2018-12-18 12:52:08 -0800

    update

--- zillow.py
+++ zillow.py
@@ -3,7 +3,7 @@ from pyzillow.pyzillow import ZillowWrapper, GetDeepSearchResults
 address = '1267 via caliente'
 zipcode = '92069'
 
-zillow_data = ZillowWrapper('X1-ZWz187irghgpaj_17jma')
+zillow_data = ZillowWrapper('')
 deep_search_response = zillow_data.get_deep_search_results(address, zipcode)
 result = GetDeepSearchResults(deep_search_response)
 
